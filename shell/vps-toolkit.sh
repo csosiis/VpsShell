370e168bf68a2b3d3ed552e8eee18e72cc498ab7
csosiis <csos@vip.qq.com>
2025-07-11 19:58:59 +0800

    o

--- shell/vps-toolkit.sh
+++ shell/vps-toolkit.sh
@@ -2068,7 +2068,7 @@ nezha_agent_menu() {
         if is_nezha_agent_v3_installed; then
             echo -e "$CYAN║$NC   5. 安装/重装 Phoenix V0 探针 ${GREEN}(已安装)$NC           $CYAN║$NC"
         else
-            echo -e "$CYAN║$NC   5. 安装/重装 Phoenix V0 探针 ${YELLOW}(未安装)$NC           $CYAN║$NC"
+            echo -e "$CYAN║$NC   5. 安装/重装 Phoenix V0 探针 ${YELLOW}(未安装)$NC          $CYAN║$NC"
         fi
         echo -e "$CYAN║$NC                                                  $CYAN║$NC"
         echo -e "$CYAN║$NC   6. $RED卸载 Phoenix V0 探针$NC                        $CYAN║$NC"
